4d0a760d46039d988fdf4ecd25b91b936f2fd9b0
Ksenia <ks.markodev@gmail.com>
2018-04-23 00:06:00 +0300

    Web Api simple functional (changes)

--- simplewebapi2.ninject/SimpleWebApi2.Ninject/Controllers/ValuesController.cs
+++ simplewebapi2.ninject/SimpleWebApi2.Ninject/Controllers/ValuesController.cs
@@ -1,21 +1,31 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
+using System.Web.Mvc;
+using AutoMapper;
+using BLL.DTO;
 using BLL.Interfaces;
 using SimpleWebApi2.Ninject.Business;
+using SimpleWebApi2.Ninject.Models;
 
 namespace SimpleWebApi2.Ninject.Controllers
 {
     public class ValuesController : ApiController
     {
-        private readonly IQuestService qs;
+        private readonly IQuestService questService;
         public ValuesController(IQuestService questService)
         {
-            qs = questService;
+            this.questService = questService;
         }
 
-        public int Get()
+        public IEnumerable<Quest> GetQuests()
         {
-            return qs.GetAll().Count();
+            return Mapper.Map<IEnumerable<QuestDTO>, IEnumerable<Quest>>(questService.GetAll());
+        }
+
+        public Quest Get(int id)
+        {
+            return Mapper.Map<QuestDTO, Quest>(questService.Find(id));
         }
     }
 }
